eadbe325e30eb470b83c1bbafdd308c676e698fc
でんのこ <dennnoko314@gmail.com>
2023-03-16 23:32:24 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jettodoapp/ui/theme/Task.kt
@@ -0,0 +1,11 @@
+package com.example.jettodoapp.ui.theme
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id : Int = 0,
+    var title : String,
+    var description : String,
+)
